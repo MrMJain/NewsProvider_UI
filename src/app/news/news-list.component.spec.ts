9a956c59a4b402e40dfd08fed507c1dd22c74356
Manish Jain1 <Manish.Jain1@rsystems.com>
2025-02-27 14:45:10 +0530

    Base UI project commit
    UI to fetch news

---
+++ src/app/news/news-list.component.spec.ts
@@ -0,0 +1,161 @@
+import { TestBed } from '@angular/core/testing';
+import { NewsListComponent } from './news-list.component';
+import { NewsService } from '../core/services/news-service.service';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { pipe,Observable, of, Subject } from 'rxjs';
+import { NewsResponse } from '../model/news';
+import { pageparam } from '../model/pageparam';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { EventEmitter } from '@angular/core';
+ 
+describe('NewsListComponent', () => {
+ 
+  let fixture :any;
+    let app : any;
+    let service :  NewsService
+    let pageEmitter: EventEmitter<PageEvent>;
+    let mockPaginator: Partial<MatPaginator>;
+    const mockNewsResponse: NewsResponse = {
+      results: [
+        { id: 1, title: 'Test News 1' },
+        { id: 2, title: 'Test News 2' },
+      ],
+      pageInfo: {
+        totalCount: 2,
+        pageSize: 1,
+        totalPages: 0,
+        isFullDataRequired: false
+      },
+      isSuccess: false,
+      message: ''
+    };
+     
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule,FormsModule,BrowserAnimationsModule,NoopAnimationsModule,
+        MatFormFieldModule,MatPaginatorModule,MatButtonModule,MatProgressBarModule, MatInputModule,MatProgressSpinnerModule,MatFormField,MatTableModule
+      ],
+     declarations: [NewsListComponent],
+      providers:[HttpClient,{provide: NewsService, useValue: { getNews: () => of(''), instant: () => of('') }},   { 
+          provide: MatPaginator, 
+          useValue: { 
+            page: pageEmitter,
+            pageIndex: 0,
+            pageSize: 10,
+            length: 100,
+          } 
+        }]
+    })
+    .compileComponents();
+    fixture = TestBed.createComponent(NewsListComponent);
+    app = fixture.componentInstance;
+    service = fixture.debugElement.injector.get(NewsService);
+    pageEmitter = new EventEmitter<PageEvent>();
+    mockPaginator = {
+      page: pageEmitter as any, // Mock the page EventEmitter
+      pageIndex: 0,
+      pageSize: 10,
+      length: 10,
+    };
+    
+    app.filterValue = '';
+    app.paginator = mockPaginator as MatPaginator;;
+  });
+  
+  it('should create', () => {
+    expect(NewsListComponent).toBeTruthy();
+  });
+  it('should return  List of News',()=>{
+    const mockNewsRequest: pageparam= {
+      pageSize : 10,
+      pageNumber : 1,
+      isFullDataRequired : false,
+      search : ''
+    };
+    spyOn(service,"getNews").and.callFake(() => {
+      return of(mockNewsResponse);
+    });
+    app.onLoad().subscribe((newsData: NewsResponse) => expect(newsData.results.length).toBeGreaterThan(0)); 
+      
+  });
+  it('should call getNews$ and update dataSource on ngAfterViewInit', () => {
+    const mockNewsResponse1: NewsResponse = {
+      results: [
+        { id: 1, title: 'Test News 1' },
+        { id: 2, title: 'Test News 2' },
+      ],
+      pageInfo: {
+        totalCount: 2,
+        pageSize: 1,
+        totalPages: 0,
+        isFullDataRequired: false
+      },
+      isSuccess: false,
+      message: ''
+    };
+    let pageInfo: pageparam = new pageparam();
+    pageInfo.pageNumber = 1;
+    pageInfo.pageSize = 5;
+    pageInfo.search ='';
+    pageInfo.isFullDataRequired = false;
+    spyOn(service, 'getNews').and.returnValue(of(mockNewsResponse));
+    // spyOn(service,"getNews").and.callFake(() => {
+    //   return of(mockNewsResponse);
+    // });
+    fixture.componentInstance.ngAfterViewInit(); // triggers ngAfterViewInit
+    fixture.detectChanges();
+
+    expect(service.getNews).toHaveBeenCalledWith(pageInfo);
+    expect(app.News.length).toBe(2);
+    expect(app.isLoading).toBeFalse(); 
+  });
+  
+  it('should apply filter and load data if filter length > 3', () => {
+    const mockNewsResponse2: NewsResponse = {
+      results: [
+        { id: 1, title: 'Test News 1' },
+        { id: 2, title: 'Test News 2' },
+      ],
+      pageInfo: {
+        totalCount: 2,
+        pageSize: 1,
+        totalPages: 0,
+        isFullDataRequired: false
+      },
+      isSuccess: false,
+      message: ''
+    };
+    // Arrange
+    const event = { target: { value: 'Test' } };
+          let pageInfo: pageparam = new pageparam();
+          pageInfo.pageNumber = 1;
+          pageInfo.pageSize = 10;
+          pageInfo.search ='test';
+          pageInfo.isFullDataRequired = false;
+    // Act
+     //Emit a PageEvent to simulate paginator interaction
+   pageEmitter.emit({
+    pageIndex: 0,
+    pageSize: 10,
+    length: 20,
+  });
+    spyOn(service,"getNews").and.callFake(() => {
+            return of(mockNewsResponse2);
+          });
+         
+    app.applyFilter(event);
+   
+    // Assert
+    expect(service.getNews).toHaveBeenCalledWith(pageInfo);
+    expect(app.News.length).toEqual(2);
+    expect(service.getNews).toHaveBeenCalled();
+  });
+});
---
+++ src/app/news/news-list.component.ts
@@ -0,0 +1,91 @@
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { News, NewsResponse } from '../model/news';
+import { MatPaginator } from '@angular/material/paginator';
+
+import { MatSelectChange } from '@angular/material/select';
+import { MatSort, Sort } from '@angular/material/sort';
+import { NewsService } from '../core/services/news-service.service';
+import { merge, Observable, of as observableOf, pipe ,Subscription} from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+
+import  {pageparam} from '../model/pageparam';
+@Component({
+  selector: 'app-news-list',
+  templateUrl: './news-list.component.html',
+  styleUrl: './news-list.component.css'
+})
+export class NewsListComponent  implements OnInit {
+  pageInfo: pageparam = new pageparam();
+  newsResponse : NewsResponse = new NewsResponse;
+  displayedColumns: string[] = [
+    'id',
+    'title',
+  ];
+  dataSource = new MatTableDataSource<News>();
+  dataSourceWithPageSize = new MatTableDataSource(this.newsResponse.results);
+  isLoading = false;
+  totalData: number;
+  filterValue: string = "";
+  News: News[];
+  constructor(private _newsService: NewsService,private cdr: ChangeDetectorRef) {}
+
+  @ViewChild('paginator') paginator: MatPaginator;
+  @ViewChild('paginatorPageSize')                                                                     
+  paginatorPageSize!: MatPaginator;
+
+  pageSizes = [5, 10, 50];
+
+  ngOnInit(): void {}
+  onLoad() {
+    this.pageInfo.pageSize =  this.paginator.pageSize;
+    this.pageInfo.pageNumber =  this.paginator.pageIndex + 1;
+    this.pageInfo.search = this.filterValue;
+    this.pageInfo.isFullDataRequired = false;
+    return this._newsService.getNews(this.pageInfo)
+  }
+  applyFilter(event: Event) {
+    this.filterValue =  (event.target as HTMLInputElement).value;  
+    if(this.filterValue.length > 3)
+    {
+      this.filterValue = this.filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+      this.loadNewsList();
+    }
+    else if(this.filterValue.length ==0){
+      this.Clear();
+    }
+  }
+  ClearSearch(){
+    this.Clear();
+}
+  ngAfterViewInit() {
+    this.loadNewsList()
+    this.cdr.detectChanges();
+  }     
+  Clear(){
+    this.filterValue = ""; // Datasource defaults to lowercase matches
+    this.loadNewsList();
+  }
+  
+  loadNewsList = (): void => {
+    this.paginator.page
+    .pipe(
+      startWith({}),
+      switchMap((r : any) => {
+        this.isLoading = true;
+        return this.onLoad()
+        .pipe(catchError(() => observableOf(null)));
+      }),
+      map((newsData) => {
+        if (newsData == null && newsData == undefined) return [];
+        this.totalData = newsData.pageInfo.totalCount;
+        this.isLoading = false;
+        return newsData.results;
+      })
+    )
+    .subscribe((newsData) => {
+      this.News = newsData;
+      this.dataSource = new MatTableDataSource(this.News);
+    });
+  };
+}
